fb7248d1b4f17464c6640b8e0e8873fc5622fdbc
AnuShrestha08 <swiftechanushrestha@gmail.com>
2023-04-19 15:39:34 +0545

    First fragment work done

--- DataPassToFragments/app/src/main/java/com/swift/datapasstofragments/Fragment1.kt
+++ DataPassToFragments/app/src/main/java/com/swift/datapasstofragments/Fragment1.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 
 
 class Fragment1 : Fragment() {
@@ -14,7 +16,24 @@ class Fragment1 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false)
+        val view=  inflater.inflate(R.layout.fragment_1, container, false)
+        val button : Button = view.findViewById(R.id.fragment2_btn)
+        button.setOnClickListener {
+            val edittext: EditText = view.findViewById(R.id.enter_text)
+            val input = edittext.text.toString()
+            val bundle = Bundle()
+            bundle.putString("data",input)
+
+            val fragment = Fragment2()
+            fragment.arguments = bundle
+
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, fragment).commit()
+            }
+
+
+        }
+        return view
     }
 
 
